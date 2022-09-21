972fd9856c330465ab687bd50f35ad5a8bceca0e
netuh <waldemar.neto@gmail.com>
2022-09-21 11:47:34 -0300

    com acesso a banco mínimo

--- app.py
+++ app.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+dicionario = {
+    "neto": "123",
+    "luis": "456"
+}
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route('/create/<first_name>/<last_name>')
+def create(first_name='nome', last_name='nome2'):
+    return 'Hello ' + first_name + ',' + last_name
+
+
+@app.route("/login2")
+def login2():
+    return dicionario["luis"]
 
 
 @app.route("/home")
